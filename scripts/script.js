93fe4a401f1ddf5b121e6e58b3bbb2fef0ba9209
TotalDomination-byte <namsrai.batkhaan@gmail.com>
2022-04-05 20:02:03 +0800

    day 2

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+const navLabel = document.querySelector(".nav-label");
+const navMenu = document.getElementById('nav-menu');
+const header = document.querySelector('.header');
+navLabel.addEventListener('click', () => {
+    navMenu.classList.toggle('animation');
+})
+
+// ---------------HEADER BACKGROUND WHEN SCROLLING---------------
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    if (scrollY >= 30) {
+        header.classList.add("animation");
+    } else {
+        header.classList.remove("animation");
+    }
+})
+
+// -------------CLOSE MENU WHEN SOMETHING IS CLICKED-------------
+
+const navLinks = document.querySelectorAll('.nav__link');
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', () => {
+        navMenu.classList.remove('animation');
+    })
+}
+window.addEventListener('click', (e) => {
+    const navList = document.querySelector('.nav__list');
+    const navLabel = document.querySelector('.nav-label');
+    const navInput = document.getElementById('nav-input');
+    if (e.target !== navLabel && e.target !== navInput) {
+        navMenu.classList.remove('animation');
+    }
+})
+
+// -----------------------HOME SWIPER-----------------------
+
+let homeSwiper = new Swiper (".homeSwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+})
