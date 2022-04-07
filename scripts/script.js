bb489a97d5840eb2a73997dcf04f6da9b4fee3d1
TotalDomination-byte <namsrai.batkhaan@gmail.com>
2022-04-07 23:37:52 +0800

    day 4

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,18 @@
-const navLabel = document.querySelector(".nav-label");
+const navMenuIcon = document.getElementById("nav-menu-icon");
 const navMenu = document.getElementById('nav-menu');
 const header = document.querySelector('.header');
-navLabel.addEventListener('click', () => {
-    navMenu.classList.toggle('animation');
+console.log(navMenuIcon);
+navMenuIcon.addEventListener('click', () => {
+    if (navMenuIcon.classList.contains('animation')) {
+        navMenuIcon.classList.add("animation1");
+        navMenuIcon.classList.remove("animation");
+    } else if (navMenuIcon.classList.contains('animation1')) {
+        navMenuIcon.classList.add("animation");
+        navMenuIcon.classList.remove("animation1");
+    } else {
+        navMenuIcon.classList.add("animation");
+    }
+    navMenu.classList.toggle('nm-animation');
 })
 
 // ---------------HEADER BACKGROUND WHEN SCROLLING---------------
@@ -24,14 +34,6 @@ for (let i = 0; i < navLinks.length; i++) {
         navMenu.classList.remove('animation');
     })
 }
-window.addEventListener('click', (e) => {
-    const navList = document.querySelector('.nav__list');
-    const navLabel = document.querySelector('.nav-label');
-    const navInput = document.getElementById('nav-input');
-    if (e.target !== navLabel && e.target !== navInput) {
-        navMenu.classList.remove('animation');
-    }
-})
 
 // -----------------------HOME SWIPER-----------------------
 
@@ -52,7 +54,18 @@ let testimonialsSwiper = new Swiper (".testimonialsSwiper", {
     },
     spaceBetween: 3,
     breakpoints: {
-        
+        600: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
     }
 })
 
@@ -62,5 +75,15 @@ let blogsSwiper = new Swiper (".blogsSwiper", {
     spaceBetween: 20,
     pagination: {
         el: ".swiper-pagination",
+    },
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
     },
 })
